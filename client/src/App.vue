db2e4d840a291e73eba885cecd5d4a06739e4980
Normano64 <per.bergqwist@gmail.com>
2016-04-25 09:13:39 +0200

    Refactored scss for the modals

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,8 +3,8 @@
     <div id="main">
         <router-view></router-view>
     </div>
-    <nick-modal-partial v-if="nickModal" transition="top"></nick-modal-partial>
-    <error-modal-partial v-if="errorModal" transition="top" :message="errorMessage"></error-modal-partial>
+    <nick-modal-partial class="modal" v-if="nickModal" transition="top"></nick-modal-partial>
+    <error-modal-partial class="modal" v-if="errorModal" transition="top" :message="errorMessage"></error-modal-partial>
     <overlay-partial v-if="overlay" transition="opacity" :click-listener="overlayClickListener"></overlay-partial>
 </template>
 
@@ -109,6 +109,42 @@
      @include respond-to(small-medium) {
          width:100%;
          margin:0;
+     }
+ }
+ .modal {
+     position:fixed;
+     max-width:300px;
+     width:100%;
+     margin:0 auto;
+     left:0;
+     right:0;
+     padding:12px 18px;
+     top:120px;
+     background:white;
+     box-shadow:0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);
+     z-index:100;
+     h2 {
+         text-align:center;
+     }
+     input[type=button] {
+         width:calc(50% - 6px);
+         float:left;
+         margin-right:6px;
+         background:$gray;
+     }
+     input[type=submit] {
+         width:calc(50% - 6px);
+         float:left;
+         margin-left:6px;
+     }
+     &.top-transition {
+         transition:all .3s ease;
+         transform:translateY(0px);
+         opacity:1;
+     }
+     &.top-enter, &.top-leave {
+         transform:translateY(-200px);
+         opacity:0;
      }
  }
 </style>
--- client/src/components/partials/NickModal.vue
+++ client/src/components/partials/NickModal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="modal">
+    <div>
         <h2>Change nickname</h2>
         <form @submit.prevent="changeNick">
             <input type="text" v-model="nick" v-el:nick placeholder="Nickname" @keyup.esc="$dispatch('hideNickModal')"/>
@@ -42,41 +42,4 @@
 <style lang="sass">
  @import "variables";
  @import 'mixin';
- 
- .modal {
-     position:fixed;
-     max-width:300px;
-     width:100%;
-     margin:0 auto;
-     left:0;
-     right:0;
-     padding:12px 18px;
-     top:120px;
-     background:white;
-     box-shadow:0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);
-     z-index:100;
-     h2 {
-         text-align:center;
-     }
-     input[type=button] {
-         width:calc(50% - 6px);
-         float:left;
-         margin-right:6px;
-         background:$gray;
-     }
-     input[type=submit] {
-         width:calc(50% - 6px);
-         float:left;
-         margin-left:6px;
-     }
-     &.top-transition {
-         transition:all .3s ease;
-         transform:translateY(0px);
-         opacity:1;
-     }
-     &.top-enter, &.top-leave {
-         transform:translateY(-200px);
-         opacity:0;
-     }
- }
 </style>
